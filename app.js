8f03bd524e40f93c25860d8e3e5ef5cf0fb69491
johnathanjohnathan <johnathanvingttrois@gmail.com>
2024-05-29 19:24:48 +0700

    adding morgan

--- app.js
+++ app.js
@@ -2,19 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = require('./routes')
-const { Sequelize } = require('sequelize')
 const morgan = require('morgan')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(morgan('common'))
 app.use(router)
-app.use(morgan('tiny'))
-
-
-const sequelize = new Sequelize('db_movies', 'postgres', '12345678', {
-    host: 'localhost',
-    dialect: 'postgres'
-  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
